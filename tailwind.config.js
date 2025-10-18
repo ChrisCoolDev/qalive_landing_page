699bb99cb9aae60b5a9fda8c6c2c862b749776db
ChrisCoolDev <talchrist09@gmail.com>
2025-10-19 01:22:23 +0300

    preparing for deployment

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue}", // parcours tous les fichiers Astro/Vue/JS
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        anton : ['anton', 'sans-serif'],
+        nunito : ['nunito', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
